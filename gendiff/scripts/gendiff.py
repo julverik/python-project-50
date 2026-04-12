4e51709dc4589fa2852dcc45c26ac05073c345e9
zxcwave <90509123+julverik@users.noreply.github.com>
2026-04-12 16:57:08 +0300

    step 2: add --format option and uv build

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,18 +1,25 @@
-#!/usr/bin/env python3
 import argparse
+
 
 def main():
     parser = argparse.ArgumentParser(
         description="Compares two configuration files and shows a difference."
     )
-    parser.add_argument("first_file", help="Path to first file")
-    parser.add_argument("second_file", help="Path to second file")
-    
+    parser.add_argument("first_file")
+    parser.add_argument("second_file")
+    parser.add_argument(
+        "-f", "--format",
+        help="set format of output"
+    )
+
     args = parser.parse_args()
-    
-    # Пока просто заглушка
+
+    # Пока заглушка
     print(f"First file: {args.first_file}")
     print(f"Second file: {args.second_file}")
+    if args.format:
+        print(f"Format: {args.format}")
+
 
 if __name__ == "__main__":
     main()
